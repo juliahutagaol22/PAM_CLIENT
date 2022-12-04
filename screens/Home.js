a24578accce7496a2649a8ee6f658fa54130a01d
MuhammadAnwarIbrahim <91143815+MuhammadAnwarIbrahim@users.noreply.github.com>
2022-12-05 01:56:52 +0700

    home fix

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,7 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Home({ navigation }) {
-  
   const dashboard = () => {
     navigation.navigate('Login')
   }
@@ -17,26 +16,40 @@ export default function Home({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={dashboard}
-      >
-        <Text>Dashboard Admin</Text>
-      </TouchableOpacity>
+      <ImageBackground source={{ uri: 'https://image.shutterstock.com/image-photo/blurred-interior-public-library-books-260nw-1896648337.jpg' }} resizeMode="cover" style={styles.image}>
+        <Text style={styles.title}>Sistem Arsip Surat</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={dashboard}
+        >
+          <Ionicons name="home" size={32} color="black" />
+          <Text style={styles.text}>Dashboard Admin</Text>
+          <Text>Halaman Awal</Text>
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={suratMasuk}
-      >
-        <Text>Surat Masuk</Text>
-      </TouchableOpacity> 
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={suratKeluar}
-      >
-        <Text>Surat Keluar</Text>
-      </TouchableOpacity> 
+        <TouchableOpacity
+          style={styles.button}
+          onPress={suratMasuk}
+        >
+          <MaterialCommunityIcons name="email-receive" size={32} color="black" />
+          <Text style={styles.text}>Surat Masuk</Text>
+          <Text>lihat surat Masuk </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={suratKeluar}
+        >
+          <MaterialCommunityIcons name="email-send" size={32} color="black" />
+          <Text style={styles.text}>Surat Keluar</Text>
+          <Text>lihat surat Keluar </Text>
+
+        </TouchableOpacity>
+      </ImageBackground>
+
+
+
 
     </View>
   );
@@ -46,12 +59,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
+    // backgroundImage: `url("https://pbs.twimg.com/profile_images/758612085194432512/UwFUDaB4_400x400.jpg")`,
+    //backgroundRepeat: 'no-repeat',
+
   },
   button: {
     alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
+    alignSelf: "auto",
+    backgroundColor: "white",
+    padding: 15,
+    borderColor: "#555555",
+    borderWidth: 1,
+    borderRadius: 0,
+    marginTop: 20,
+    borderEndWidth: 10,
+    marginHorizontal: 60,
+    opacity: 0.8,
+    borderRadius: 200 / 2,
+
   },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+
+
+  image: {
+    justifyContent: "center",
+    width: 400,
+    height: 800,
+    position: "relative"
+  },
+
 });
